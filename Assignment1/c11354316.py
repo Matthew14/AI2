924591f24e16b36fa756e28896535b3133e7cfd5
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-02-15 16:46:31 +0000

    identifying cont and cat

--- Assignment1/c11354316.py
+++ Assignment1/c11354316.py
@@ -6,7 +6,7 @@ AI2 Assignment 1
 
 import csv
 import os
-import numpy
+import numpy as np
 from functools import wraps
 
 
@@ -23,14 +23,42 @@ def check_file_exists(f):
 @check_file_exists
 def get_feature_names(file_path):
     with open(file_path, 'r') as f:
-        return [l.rstrip() for l in f.readlines() if l != '\n']
+        return [line.strip() for line in f.readlines() if line.strip() != '\n']
 
 
 @check_file_exists
 def load_data_set(file_path):
     with open(file_path, 'r') as f:
         reader = csv.reader(f, delimiter = ',')
-        return numpy.array(list(reader))
+        return np.array(list(reader))
+
+
+def is_numeric(in_string):
+    try:
+        float(in_string)
+        return True
+    except:
+        return False
+
+
+def identify_cont_and_cat(data_set):
+    cont_cat_dict = dict()
+    cont = 'continuous'
+    cat = 'categorical'
+    full_line_index = -1
+
+    for i in range(len(data_set)):
+        for j in range(len(data_set[i])):
+            if data_set[i][j].strip() == '?':
+                break
+        else:
+            full_line_index = i
+            break
+
+    for i in range(len(data_set[full_line_index])):
+        cont_cat_dict[i] = cont if is_numeric(data_set[full_line_index][i].strip()) else cat
+
+    return cont_cat_dict
 
 
 def output_cont_report():
@@ -47,7 +75,8 @@ if __name__ == "__main__":
     data_set_file = './data/DataSet.txt'
     feature_names_file = './data/featurenames.txt'
 
-    # ds = load_data_set(data_set_file)
+    data_set = load_data_set(data_set_file)
+    feature_names = get_feature_names(feature_names_file)
+    no_features = len(feature_names)
 
-    get_feature_names(feature_names_file)
-
+    assert len(data_set[0]) == no_features
