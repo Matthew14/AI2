0dd112383a9496d52e7f9123baaeea1622d62b90
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-02-15 15:16:05 +0000

    initial report

--- Assignment1/c11354316.py
+++ Assignment1/c11354316.py
@@ -1,8 +1,22 @@
-# AI2 Assignment 1
-# Matthew O'Neill
-# C11354316
-# 16/02/15
+"""
+AI2 Assignment 1
+Matthew O'Neill/C11354316
+16/02/15
+"""
 
-dataSetFile = './data/DataSet.txt'
-featureNamesFile = './data/featurenames.txt'
+import csv
+import os
+import numpy
+
+
+data_set_file = './data/DataSet.txt'
+feature_names_file = './data/featurenames.txt'
+
+
+def get_feature_names(file_path):
+    if not os.path.isfile(file_path):
+        raise IOError('file doesn\'t exist')
+
+    with open(file_path, 'r') as f:
+        return [l.rstrip() for l in f.readlines() if l != '\n']
 
