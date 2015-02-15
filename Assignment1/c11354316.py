d304aca99b90ad1cb40672d11963832591a51ed1
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-02-15 15:46:44 +0000

    check file exists wrapper

--- Assignment1/c11354316.py
+++ Assignment1/c11354316.py
@@ -7,16 +7,26 @@ AI2 Assignment 1
 import csv
 import os
 import numpy
+from functools import wraps
 
 
+#function wrapper to check first parameter of a func is a file that exists
+def check_file_exists(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if len(args) > 0 and not os.path.isfile(args[0]):
+            raise IOError('file doesn\'t exist')
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@check_file_exists
 def get_feature_names(file_path):
-    if not os.path.isfile(file_path):
-        raise IOError('file doesn\'t exist')
-
     with open(file_path, 'r') as f:
         return [l.rstrip() for l in f.readlines() if l != '\n']
 
 
+@check_file_exists
 def load_data_set(file_path):
     with open(file_path, 'r') as f:
         reader = csv.reader(f, delimiter = ',')
@@ -37,4 +47,7 @@ if __name__ == "__main__":
     data_set_file = './data/DataSet.txt'
     feature_names_file = './data/featurenames.txt'
 
-    ds = load_data_set(data_set_file)
+    # ds = load_data_set(data_set_file)
+
+    get_feature_names(feature_names_file)
+
