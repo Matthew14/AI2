7fa64b7e99a0f9e277697fdabc57084a2476bced
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-02-15 18:41:49 +0000

    continuous features done

--- Assignment1/c11354316.py
+++ Assignment1/c11354316.py
@@ -8,7 +8,7 @@ import csv
 import os
 import numpy as np
 from functools import wraps
-
+from collections import OrderedDict
 
 #function wrapper to check first parameter of a func is a file that exists
 def check_file_exists(f):
@@ -23,7 +23,7 @@ def check_file_exists(f):
 @check_file_exists
 def get_feature_names(file_path):
     with open(file_path, 'r') as f:
-        return [line.strip() for line in f.readlines() if line.strip() != '\n']
+        return [line.strip() for line in f.readlines() if line.strip() != '']
 
 
 @check_file_exists
@@ -42,7 +42,7 @@ def is_numeric(in_string):
 
 
 def identify_cont_and_cat(data_set):
-    cont_cat_dict = dict()
+    cont_and_cats = dict()
     cont = 'continuous'
     cat = 'categorical'
     full_line_index = -1
@@ -56,19 +56,70 @@ def identify_cont_and_cat(data_set):
             break
 
     for i in range(len(data_set[full_line_index])):
-        cont_cat_dict[i] = cont if is_numeric(data_set[full_line_index][i].strip()) else cat
+        cont_and_cats[i] = cont if is_numeric(data_set[full_line_index][i].strip()) else cat
 
-    return cont_cat_dict
+    conts = [k for k in cont_and_cats.keys() if cont_and_cats[k] == 'continuous']
+    cats = [k for k in cont_and_cats.keys() if cont_and_cats[k] == 'categorical']
 
-
-def output_cont_report():
-    outfile = 'c11354316CONT.csv'
-    pass #TODO
+    return conts, cats
 
 
-def output_cat_report():
-    outfile = 'c11354316CAT.csv'
-    pass #TODO
+def output_report(feature_dict, outfile):
+
+    with open(outfile, 'w') as f:
+        writer = csv.writer(f)
+        headings = ['FEATURENAME']
+        headings.extend(list(feature_dict[list(feature_dict.keys())[0]].keys()))
+
+        writer.writerow(headings)
+
+        for k, v in feature_dict.items():
+            fn = k
+            vals = v.values()
+            row = [k]
+            row.extend(list(vals))
+            writer.writerow(row)
+
+
+def create_and_output_cont_report(data_set, conts, feature_names):
+    outfile = './data/c11354316CONT.csv'
+
+    data_set_len = len(data_set)
+    feature_dict = OrderedDict()
+
+    for feature in conts:
+        feature_name = feature_names[feature]
+
+        feature_dict[feature_name] = OrderedDict()
+        feature_dict[feature_name]['missing_vals'] = 0
+
+        #convert from string type to floats for this numeric col
+        col = np.array(data_set[:, feature], dtype='float')
+        non_zero_col = col[np.nonzero(col)] #remove 0 (missing)
+
+        for i in range (len(col)):
+            if col[i] == 0:
+                feature_dict[feature_name]['missing_vals'] += 1
+
+        feature_dict[feature_name]['min'] = np.min(non_zero_col)
+        feature_dict[feature_name]['max'] = np.max(non_zero_col)
+        feature_dict[feature_name]['mean'] = np.mean(non_zero_col)
+        feature_dict[feature_name]['median'] = np.median(non_zero_col)
+        feature_dict[feature_name]['std'] = np.std(non_zero_col)
+        feature_dict[feature_name]['first_quartile'] = np.percentile(non_zero_col, 25)
+        feature_dict[feature_name]['third_quartile'] = np.percentile(non_zero_col, 75)
+        feature_dict[feature_name]['no_distinct'] = len(np.unique(non_zero_col))
+        feature_dict[feature_name]['percent_missing'] = (feature_dict[feature_name]['missing_vals'] * 100) / len(col)
+
+    output_report(feature_dict, outfile)
+
+
+def create_and_output_cat_report(data_set, cats, feature_names):
+    outfile = './data/c11354316CAT.csv'
+
+    for feature in cats:
+        feature_name = feature_names[feature]
+
 
 
 if __name__ == "__main__":
@@ -80,3 +131,8 @@ if __name__ == "__main__":
     no_features = len(feature_names)
 
     assert len(data_set[0]) == no_features
+
+    conts, cats = identify_cont_and_cat(data_set)
+
+    create_and_output_cont_report(data_set, conts, feature_names)
+    create_and_output_cat_report(data_set, cats, feature_names)
