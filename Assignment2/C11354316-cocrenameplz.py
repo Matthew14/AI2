a697666f78e161cce2232827f94c94ec782bd2ba
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-03-27 18:04:06 +0000

    data files and requirements

---
+++ Assignment2/C11354316-cocrenameplz.py
@@ -0,0 +1,16 @@
+'''
+Artificial Inteligence 2 Assignment
+Matthew O'Neill / C11354316 + Colin Delaney / numberhere
+'''
+
+import os
+import numpy
+
+
+data_dir = "data"
+queries_file_path = os.path.join(data_dir, 'queries.txt')
+training_set_file_path = os.path.join(data_dir, 'trainingset.txt')
+
+
+if __name__ == '__main__':
+    pass
