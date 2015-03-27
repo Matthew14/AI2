115b325dfe8df8366d7d1fb02c478b8790f07b03
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-03-27 18:12:00 +0000

    check files exist

--- Assignment2/C11354316-cocrenameplz.py
+++ Assignment2/C11354316-cocrenameplz.py
@@ -4,12 +4,17 @@ Matthew O'Neill / C11354316 + Colin Delaney / numberhere
 '''
 
 import os
+import sys
 import numpy
 
 
 data_dir = "data"
 queries_file_path = os.path.join(data_dir, 'queries.txt')
 training_set_file_path = os.path.join(data_dir, 'trainingset.txt')
+
+if not os.path.isfile(queries_file_path) or not os.path.isfile(training_set_file_path):
+    print "data file(s) not found..."
+    sys.exit(1)
 
 
 if __name__ == '__main__':
