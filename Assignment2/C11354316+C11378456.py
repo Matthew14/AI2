87481885bbc8bcc900dedd3bd509b8a9039d1a4a
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-04-12 14:24:36 +0100

    works with python 3 now

--- Assignment2/C11354316+C11378456.py
+++ Assignment2/C11354316+C11378456.py
@@ -9,11 +9,19 @@ import numpy
 
 
 data_dir = "data"
+solutions_dir = "solutions"
+
+if not os.path.isdir(data_dir):
+    os.mkdir(data_dir)
+if not os.path.isdir(solutions_dir):
+    os.mkdir(solutions_dir)
+
 queries_file_path = os.path.join(data_dir, 'queries.txt')
 training_set_file_path = os.path.join(data_dir, 'trainingset.txt')
+solutions_file_path = os.path.join(solutions_dir, 'C11354316+C11378456.txt')
 
 if not os.path.isfile(queries_file_path) or not os.path.isfile(training_set_file_path):
-    print "data file(s) not found..."
+    print ("data file(s) not found...")
     sys.exit(1)
 
 
