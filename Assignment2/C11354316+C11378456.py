62b77e6c08bbd1474d8e20271c654363f7f63cca
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-04-19 19:56:44 +0100

    done

--- Assignment2/C11354316+C11378456.py
+++ Assignment2/C11354316+C11378456.py
@@ -6,11 +6,8 @@ Matthew O'Neill / C11354316 + Colin Delaney / C11378456
 import os
 import sys
 import numpy as np
-import pandas as pd
-import math
-from sklearn.feature_extraction import DictVectorizer
 from sklearn import tree
-from sklearn import cross_validation
+
 
 #globals
 data_dir ="data"
@@ -33,51 +30,30 @@ def setup_files():
         sys.exit(1)
 
 
-def get_headings():
-    headings = ['id','Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Rawah_Wilderness_Area', 'Neota_Wilderness_Area', 'Comanche_Peak_Wilderness_Area', 'Cache_la_Poudre_Wilderness_Area']
-
-    headings.extend(["Soil_Type_{}".format(i) for i in range(1, 41)])
-    headings.append('Cover_Type')
-
-    return headings
+def load_csv(fp, cols, dtype=np.float32):
+    return np.genfromtxt(fp, delimiter=',', usecols=cols, dtype=dtype)
 
 
 if __name__ == '__main__':
     setup_files()
 
-    #numeric are all headings up to Horizontal_Distance_To_Fire_Points
-    headings = get_headings()
-    numeric_headings = headings[:headings.index('Horizontal_Distance_To_Fire_Points') + 1]
-    numeric_headings.pop(0)
+    #the training data minus id and type:
+    t_num_data = load_csv(training_set_file_path, range(1, 55))
+    #the training type column
+    t_type_col = load_csv(training_set_file_path, 55, dtype=np.dtype((str, 5)))
+    #the query data minus id and type:
+    q_data = load_csv(queries_file_path, range(1, 55))
+    #the query id column
+    q_id = load_csv(queries_file_path, 0, dtype=np.dtype((str, 10)))
 
-    vectorizer = DictVectorizer( sparse = False )
-    decTreeModel = tree.DecisionTreeClassifier(criterion='entropy')
 
-    training_df = pd.read_csv(training_set_file_path, names=headings)
+    #fit data above to DTC and predict it
+    model = tree.DecisionTreeClassifier(criterion='entropy')
+    model.fit(t_num_data, t_type_col)
+    predictions = model.predict(q_data)
 
-    #numeric_cols = training_df[numeric_headings]
-    #cat_df = training_df.drop(numeric_cols, axis=1)
 
-    #cat_dic = cat_df.T.to_dict().values()
-
-    #cat_array = vectorizer.fit_transform(cat_dic)
-
-    #numeric_matrix = numeric_cols.as_matrix()
-
-    #train_dfs = np.hstack((numeric_matrix, cat_array))
-
-    #decTreeModel.fit(train_dfs, training_df['Cover_Type'])
-
-    #queries_df = pd.read_csv(queries_file_path, names=headings, nrows=100)
-    #q_num = queries_df[numeric_headings].as_matrix()
-
-    #q_cat = queries_df.drop(numeric_headings, axis=1)
-    #q_cat_dfs = q_cat.T.to_dict().values()
-    #q_vec_dfs = vectorizer.transform(q_cat_dfs)
-
-    #queries = np.hstack((q_num, q_vec_dfs ))
-    
-    #predictions = []
-
-    #for query in queries :
-    #    predictions.append(decTreeModel.predict([query]))
+    #output the predictions:
+    with open(solutions_file_path, 'w') as f:
+        for i in range(len(predictions)):
+            f.write("{},{}\n".format(q_id[i], predictions[i]))
